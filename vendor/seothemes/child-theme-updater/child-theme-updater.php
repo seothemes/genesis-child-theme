a0471e8a2ca1f3941088f5a1d01c10b15c8a3871
seothemes <seothemeswp@gmail.com>
2019-05-26 16:51:15 +0800

    Documentation

--- vendor/seothemes/child-theme-updater/child-theme-updater.php
+++ vendor/seothemes/child-theme-updater/child-theme-updater.php
@@ -1,10 +1,25 @@
 <?php
+/**
+ * Child Theme Updater.
+ *
+ * Allows modified child themes to receive automatic updates without losing changes.
+ * It works by duplicating the child theme before running an update, excluding the
+ * vendor directory. Once the update is complete, the duplicate style.css version
+ * number is updated and then all of the duplicated files are copied back to the
+ * new theme version, except for the vendor directory. Basically the only files
+ * that are updated are in the vendor directory, everything else is untouched.
+ *
+ * @package SeoThemes\ChildThemeUpdater
+ * @author  SEO Themes
+ * @license GPL-3.0-or-later
+ * @link    https://github.com/seothemes/child-theme-updater
+ */
 
 namespace SeoThemes\ChildThemeUpdater;
 
 add_action( 'upgrader_source_selection', __NAMESPACE__ . '\before_update', 10, 4 );
 /**
- * Runs before theme update.
+ * Duplicates the original theme. Runs before theme update.
  *
  * @since 1.0.0
  *
@@ -13,9 +28,11 @@ add_action( 'upgrader_source_selection', __NAMESPACE__ . '\before_update', 10, 4
  * @param \WP_Upgrader $theme_object  WP_Upgrader instance.
  * @param array        $hook_extra    Extra arguments passed to hooked filters.
  *
- * @return mixed
+ * @return string
  */
 function before_update( $source, $remote_source, $theme_object, $hook_extra ) {
+
+	// Return early if there is an error or if it's not a theme update.
 	if ( is_wp_error( $source ) || ! is_a( $theme_object, 'Theme_Upgrader' ) ) {
 		return $source;
 	}
@@ -25,7 +42,7 @@ function before_update( $source, $remote_source, $theme_object, $hook_extra ) {
 	\WP_Filesystem();
 	global $wp_filesystem;
 
-	// Duplicate theme to /temp/ directory.
+	// Duplicate theme to temp directory, excluding vendor directory.
 	$src    = \get_stylesheet_directory();
 	$target = dirname( $src ) . '/temp';
 	$skip   = apply_filters( 'child_theme_updater_skip', [ 'vendor' ] );
@@ -38,17 +55,19 @@ function before_update( $source, $remote_source, $theme_object, $hook_extra ) {
 
 add_action( 'upgrader_post_install', __NAMESPACE__ . '\after_update', 10, 3 );
 /**
- * Runs after theme update.
+ * Add customizations to new version. Runs after theme update.
  *
  * @since 1.0.0
  *
- * @param bool  $response
- * @param array $hook_extra
- * @param array $result
+ * @param bool  $response   Installation response.
+ * @param array $hook_extra Extra arguments passed to hooked filters.
+ * @param array $result     Installation result data.
  *
- * @return mixed
+ * @return bool
  */
 function after_update( $response, $hook_extra, $result ) {
+
+	// Return early if no response or destination does not exist.
 	if ( ! $response || ! array_key_exists( 'destination', $result ) ) {
 		return $response;
 	}
@@ -58,42 +77,26 @@ function after_update( $response, $hook_extra, $result ) {
 	\WP_Filesystem();
 	global $wp_filesystem;
 
-	/*
-	 * Step 1. Move new vendor directory to temp.
-	 */
-	$src  = \get_stylesheet_directory() . '/vendor';
-	$dest = dirname( dirname( $src ) ) . '/temp/vendor';
-
-	\wp_mkdir_p( $dest );
-	\copy_dir( $src, $dest );
-
-	/*
-	 * Step 2. Bump temp style sheet version.
-	 */
+	// Bump temp style sheet version.
 	$new_theme    = \get_stylesheet_directory() . '/style.css';
-	$new_data     = \get_file_data( $new_theme, [
-		'Version' => 'Version',
-	] );
+	$new_data     = \get_file_data( $new_theme, [ 'Version' => 'Version' ] );
 	$new_version  = $new_data['Version'];
 	$old_theme    = dirname( dirname( $new_theme ) ) . '/temp/style.css';
-	$old_data     = \get_file_data( $old_theme, [
-		'Version' => 'Version',
-	] );
+	$old_data     = \get_file_data( $old_theme, [ 'Version' => 'Version' ] );
 	$old_version  = $old_data['Version'];
 	$old_contents = $wp_filesystem->get_contents( $old_theme );
 	$new_contents = str_replace( $old_version, $new_version, $old_contents );
 
 	$wp_filesystem->put_contents( $old_theme, $new_contents, FS_CHMOD_FILE );
 
-	/*
-	 * Step 3. Bring everything back except vendor directory.
-	 */
+	// Bring everything back except vendor directory.
 	$target = \get_stylesheet_directory();
 	$source = dirname( $target ) . '/temp';
 	$skip   = apply_filters( 'child_theme_updater_skip', [ 'vendor' ] );
 
 	\copy_dir( $source, $target, $skip );
 
+	// Delete temp directory.
 	$wp_filesystem->delete( $source, true, 'd' );
 
 	return $response;
